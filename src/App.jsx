28634767df3014c2775acf85704e3c11f66697f1
Pranjal Gain <pranjalgain052@gmail.com>
2024-07-22 06:32:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+    const [counter, setCounter] = useState(0);
+
+    return (
+        <>
+        <p className="text-center text-3xl mt-auto">h1llo</p>
+        </>
+    );
+}
+
+export default App;
